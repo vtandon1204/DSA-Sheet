25fc0634c1c5de11d9beb89537d6d9c0e22115c1
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-06-19 23:30:33 +0530

    commit

---
+++ 6. Linked Lists/Doubly Linked List (easy)/00IntroToDoublyLL.cpp
@@ -0,0 +1,76 @@
+// Doubly Linked List: prev pointer, data, next pointer
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "X" << endl;
+}
+
+int getLength(Node *head)
+{
+    Node *temp = head;
+    int len = 0;
+    while (temp)
+    {
+        temp = temp->next;
+        len++;
+    }
+    return len;
+}
+
+void InsertAtHead(Node *&head, int data)
+{
+    Node *temp = new Node(data);
+    temp->next = head;
+    head->prev = temp;
+    head = temp;
+}
+
+void InsertAtTail(Node *&head, int data)
+{
+    Node *temp = new Node(data);
+    Node *curr = head;
+    while (curr->next != NULL)
+    {
+        curr = curr->next;
+    }
+    curr->next = temp;
+    temp->prev = curr;
+}
+int main()
+{
+    Node *node1 = new Node(5);
+    Node *head = node1;
+    print(head);
+    cout << "length of DLL is: " << getLength(head) << endl;
+    cout << "head: " << head->data << endl;
+
+    InsertAtHead(head, 7);
+    print(head);
+    cout << "head: " << head->data << endl;
+
+    InsertAtTail(head,9);
+    print(head);
+    cout << "head: " << head->data << endl;
+}
