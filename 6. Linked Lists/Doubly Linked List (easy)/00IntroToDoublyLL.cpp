3321571843239907e32457f61ada9765b23c018d
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-06-21 20:47:28 +0530

    commit

--- 6. Linked Lists/Doubly Linked List (easy)/00IntroToDoublyLL.cpp
+++ 6. Linked Lists/Doubly Linked List (easy)/00IntroToDoublyLL.cpp
@@ -41,14 +41,27 @@ int getLength(Node *head)
 
 void InsertAtHead(Node *&head, int data)
 {
-    Node *temp = new Node(data);
-    temp->next = head;
-    head->prev = temp;
-    head = temp;
+    if (head == NULL)
+    {
+        Node *temp = new Node(data);
+        head = temp;
+    }
+    else
+    {
+        Node *temp = new Node(data);
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+    }
 }
 
 void InsertAtTail(Node *&head, int data)
 {
+    if (head == NULL)
+    {
+        Node *temp = new Node(data);
+        head = temp;
+    }
     Node *temp = new Node(data);
     Node *curr = head;
     while (curr->next != NULL)
@@ -58,19 +71,50 @@ void InsertAtTail(Node *&head, int data)
     curr->next = temp;
     temp->prev = curr;
 }
+
+void InsertAtPosition(Node *head, int pos, int data)
+{
+    if (pos == 1)
+    {
+        InsertAtHead(head, data);
+        return;
+    }
+    Node *temp = new Node(data);
+    Node *curr = head;
+    int cnt = 1;
+
+    while (cnt < pos - 1)
+    {
+        curr = curr->next;
+        cnt++;
+    }
+    if (curr->next == NULL)
+    {
+        InsertAtTail(head, data);
+        return;
+    }
+    temp->next = curr->next;
+    curr->next->prev = temp;
+    curr->next = temp;
+    temp->prev = curr;
+}
 int main()
 {
-    Node *node1 = new Node(5);
-    Node *head = node1;
+    // Node *node1 = new Node(5);
+    Node *head = NULL;
     print(head);
     cout << "length of DLL is: " << getLength(head) << endl;
-    cout << "head: " << head->data << endl;
+    // cout << "head: " << head->data << endl;
 
     InsertAtHead(head, 7);
     print(head);
     cout << "head: " << head->data << endl;
 
-    InsertAtTail(head,9);
+    InsertAtTail(head, 9);
+    print(head);
+    cout << "head: " << head->data << endl;
+
+    InsertAtPosition(head, 2, 11);
     print(head);
     cout << "head: " << head->data << endl;
 }
