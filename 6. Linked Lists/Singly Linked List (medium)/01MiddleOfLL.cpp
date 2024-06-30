c4813899efa184fe7e97f7410546ea6fd89fd6ca
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-06-30 10:54:26 +0530

    commit

--- 6. Linked Lists/Singly Linked List (medium)/01MiddleOfLL.cpp
+++ 6. Linked Lists/Singly Linked List (medium)/01MiddleOfLL.cpp
@@ -1,24 +1,34 @@
 #include <bits/stdc++.h>
 ListNode *middleNode(ListNode *head)
 {
-    ListNode *tail = head;
-    int first = 0;
-    int last = 1;
-    while (tail->next != NULL)
+    // ListNode *tail = head;
+    // int first = 0;
+    // int last = 1;
+    // while (tail->next != NULL)
+    // {
+    //     tail = tail->next;
+    //     last++;
+    // }
+
+    // ListNode *midNode = head;
+    // int mid = (first + last) / 2;
+    // int cnt = 0;
+
+    // while (cnt < mid)
+    // {
+    //     midNode = midNode->next;
+    //     cnt++;
+    // }
+
+    // return midNode;
+
+    // Tortoise-Hare Method
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast != NULL && fast->next != NULL)
     {
-        tail = tail->next;
-        last++;
+        slow = slow->next;
+        fast = fast->next->next;
     }
-
-    ListNode *midNode = head;
-    int mid = (first + last) / 2;
-    int cnt = 0;
-
-    while (cnt < mid)
-    {
-        midNode = midNode->next;
-        cnt++;
-    }
-
-    return midNode;
+    return slow;
 }
