c1997d5ce695bb5e2c6dbbde8b4a4baf0c178770
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-08-29 16:39:15 +0530

    commit

--- 12. Greedy Algorithms/easy/05ValidParenthesis.cpp
+++ 12. Greedy Algorithms/easy/05ValidParenthesis.cpp
@@ -1,26 +1,51 @@
 #include <bits/stdc++.h>
-// bool check(string s, int index, int cnt)
-// {
-//     if (cnt < 0)
-//     {
+// bool check(string s, int index, int cnt) {
+//     if (cnt < 0) {
 //         return false;
 //     }
-//     if (index == s.size())
-//     {
+//     if (index == s.size()) {
 //         return (cnt == 0);
 //     }
-//     if (s[index] == '(')
-//     {
+//     if (s[index] == '(') {
 //         return check(s, index + 1, cnt + 1);
 //     }
-//     if (s[index] == ')')
-//     {
+//     if (s[index] == ')') {
 //         return check(s, index + 1, cnt - 1);
 //     }
-//     return (check(s, index + 1, cnt + 1) || check(s, index + 1, cnt - 1) ||
+//     return (check(s, index + 1, cnt + 1) || check(s, index + 1, cnt - 1)
+//     ||
 //             check(s, index + 1, cnt));
 // }
 // bool checkValidString(string s) { return check(s, 0, 0); }
-
-
-
+bool checkValidString(string s)
+{
+    int minn = 0;
+    int maxx = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(')
+        {
+            minn += 1;
+            maxx += 1;
+        }
+        else if (s[i] == ')')
+        {
+            minn -= 1;
+            maxx -= 1;
+        }
+        else
+        {
+            minn -= 1;
+            maxx += 1;
+        }
+        if (minn < 0)
+        {
+            minn = 0;
+        }
+        if (maxx < 0)
+        {
+            return false;
+        }
+    }
+    return (minn == 0);
+}
