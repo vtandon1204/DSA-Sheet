9da7e69bcc05a6d79e0c8728811f92c4fe56da1e
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-08-29 23:00:29 +0530

    commit

--- 12. Greedy Algorithms/easy/04LemonadeChange.cpp
+++ 12. Greedy Algorithms/easy/04LemonadeChange.cpp
@@ -1,1 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+bool lemonadeChange(vector<int> &bills)
+{
+    int change5 = 0;
+    int change10 = 0;
+    for (int bill : bills)
+    {
+        if (bill == 5)
+        {
+            change5++;
+        }
+        else if (bill == 10)
+        {
+            if (change5 > 0)
+            {
+                change5--;
+                change10++;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (bill == 20)
+        {
+            if (change10 > 0 && change5 > 0)
+            {
+                change10--;
+                change5--;
+            }
+            else if (change5 > 2)
+            {
+                change5 -= 3;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
