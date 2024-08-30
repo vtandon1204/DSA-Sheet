b27bd622b0c5d1bea939652f5935e94be8bf1628
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-08-30 16:55:19 +0530

    commit

--- 13. Binary Trees/Medium/05CheckForIdenticalTrees.cpp
+++ 13. Binary Trees/Medium/05CheckForIdenticalTrees.cpp
@@ -5,11 +5,7 @@ bool isIdentical(Node *r1, Node *r2)
     {
         return true;
     }
-    if (!r1 && r2)
-    {
-        return false;
-    }
-    if (r1 && !r2)
+    if (!r1 || !r2)
     {
         return false;
     }
