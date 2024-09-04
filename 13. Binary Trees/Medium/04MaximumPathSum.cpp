06fc8c96a43531188820b2e03af22284e9ce57e0
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-09-04 20:12:56 +0530

    commit

--- 13. Binary Trees/Medium/04MaximumPathSum.cpp
+++ 13. Binary Trees/Medium/04MaximumPathSum.cpp
@@ -1,1 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+int maxSum = INT_MIN;
+int func(TreeNode *root)
+{
+    if (!root)
+        return 0;
+    int leftSum = max(0, func(root->left));
+    int rightSum = max(0, func(root->right));
+
+    maxSum = max(maxSum, leftSum + rightSum + root->val);
+    int ans = root->val + max(leftSum, rightSum);
+    return ans;
+}
+int maxPathSum(TreeNode *root)
+{
+    func(root);
+    return maxSum;
+}
--- 13. Binary Trees/Medium/08VerticalOrderTraversalOfBT.cpp
+++ 13. Binary Trees/Medium/08VerticalOrderTraversalOfBT.cpp
@@ -1,1 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+vector<vector<int>> verticalTraversal(TreeNode *root)
+{
+    map<int, map<int, multiset<int>>> mp;
+    queue<pair<TreeNode *, pair<int, int>>> q;
+    q.push({root, {0, 0}});
+    while (!q.empty())
+    {
+        auto i = q.front();
+        q.pop();
+        TreeNode *node = i.first;
+        int line = i.second.first;
+        int level = i.second.second;
+        mp[line][level].insert(node->val);
+        if (node->left)
+        {
+            q.push({node->left, {line - 1, level + 1}});
+        }
+        if (node->right)
+        {
+            q.push({node->right, {line + 1, level + 1}});
+        }
+    }
+    vector<vector<int>> ans;
+    for (auto i : mp)
+    {
+        vector<int> temp;
+        for (auto j : i.second)
+        {
+            for (auto k : j.second)
+            {
+                temp.push_back(k);
+            }
+        }
+        ans.push_back(temp);
+    }
+    return ans;
+}
