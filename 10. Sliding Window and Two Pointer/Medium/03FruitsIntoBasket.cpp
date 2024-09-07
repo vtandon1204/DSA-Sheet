800fdbaed1d53fca83b98e084486d8964ce6bdf4
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-09-07 16:11:08 +0530

    commit

--- 10. Sliding Window and Two Pointer/Medium/03FruitsIntoBasket.cpp
+++ 10. Sliding Window and Two Pointer/Medium/03FruitsIntoBasket.cpp
@@ -1,26 +1,63 @@
 #include <bits/stdc++.h>
-int totalFruit(vector<int> &fruits)
+int totalFruits(vector<int> &arr)
 {
-    int left = 0, right = 0, ans = 0;
-    int n = fruits.size();
-    unordered_map<int, int> freq;
-
-    while (right < n)
+    // optimised *******
+    int left = 0;
+    int right = 0;
+    int maxLen = 0;
+    unordered_map<int, int> mpp;
+    while (right < arr.size())
     {
-        freq[fruits[right]]++;
-
-        while (freq.size() > 2)
+        mpp[arr[right]]++;
+        if (mpp.size() > 2)
         {
-            int leftKey = fruits[left];
-            freq[leftKey]--;
-            if (freq[leftKey] == 0)
-            {
-                freq.erase(leftKey);
-            }
+            mpp[arr[left]]--;
+            if (mpp[arr[left]] == 0)
+                mpp.erase(arr[left]);
             left++;
         }
-        ans = max(ans, right - left + 1);
+        else
+        {
+            maxLen = max(maxLen, right - left + 1);
+        }
         right++;
     }
-    return ans;
+    return maxLen;
+
+    // better *******
+    // int left = 0, right = 0, ans = 0;
+    // int n = arr.size();
+    // unordered_map<int, int> freq;
+
+    // while (right < n) {
+    //     freq[arr[right]]++;
+
+    //     while (freq.size() > 2) {
+    //         int leftKey = arr[left];
+    //         freq[leftKey]--;
+    //         if (freq[leftKey] == 0) {
+    //             freq.erase(leftKey);
+    //         }
+    //         left++;
+    //     }
+    //     ans = max(ans, right - left + 1);
+    //     right++;
+    // }
+    // return ans;
+
+    // brute ******
+    // int maxLen = 0;
+    // for(int i=0;i<arr.size();i++){
+    //     set<int> s;
+    //     for(int j=i;j<arr.size();j++){
+    //         s.insert(arr[j]);
+    //         if(s.size()<=2){
+    //             maxLen = max(maxLen,j-i+1);
+    //         }
+    //         else{
+    //             break;
+    //         }
+    //     }
+    // }
+    // return maxLen;
 }
