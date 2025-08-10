945c730a18ed4b0cdd3e6c8eb82434beeea95e1a
Vaibhav Tandon <vasu.tan2017@gmail.com>
2025-08-10 17:31:43 +0530

    commit

--- 15.Graphs/39FloydWarshallAlgorithm.cpp
+++ 15.Graphs/39FloydWarshallAlgorithm.cpp
@@ -5,20 +5,30 @@ using namespace std;
 // problem using Floyd Warshall algorithm
 void floydWarshall(vector<vector<int>> &dist)
 {
-    int V = dist.size();
+    int n = dist.size();
 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (dist[i][j] == -1)
+                dist[i][j] = 1e8;
+            if (i == j)
+                dist[i][j] = 0;
+        }
+    }
     // Add all vertices one by one to
     // the set of intermediate vertices.
-    for (int k = 0; k < V; k++)
+    for (int k = 0; k < n; k++)
     {
 
         // Pick all vertices as source one by one
-        for (int i = 0; i < V; i++)
+        for (int i = 0; i < n; i++)
         {
 
             // Pick all vertices as destination
             // for the above picked source
-            for (int j = 0; j < V; j++)
+            for (int j = 0; j < n; j++)
             {
 
                 // shortest path from
@@ -27,5 +37,12 @@ void floydWarshall(vector<vector<int>> &dist)
                     dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
             }
         }
+    }
+
+    // check negative cycle
+    for (int i = 0; i < n; i++)
+    {
+        if (dist[i][i] < 0)
+            cout << "negative cycle present\n";
     }
 }
